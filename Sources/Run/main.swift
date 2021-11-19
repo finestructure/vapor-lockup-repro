69b4d55b333ab2827613e645bc592a37d1f962c4
Sven A. Schmidt <sas@finestructure.co>
2021-11-19 11:36:18 +0100

    Tweak number of threads (as per Fabian's recommendation to force error sooner)

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -3,7 +3,13 @@ import Vapor
 
 var env = try Environment.detect()
 try LoggingSystem.bootstrap(from: &env)
-let app = Application(env)
-defer { app.shutdown() }
+
+let eventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+let app = Application(env, .shared(eventLoopGroup))
+
+defer {
+    app.shutdown()
+    try! eventLoopGroup.syncShutdownGracefully()
+}
 try configure(app)
 try app.run()
